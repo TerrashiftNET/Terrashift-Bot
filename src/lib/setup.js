96e93ae74fcac5c510c6678c7a5320bcf09daf0f
Terrashift <git@terrashift.net>
2023-08-22 14:44:41 +0000

    add package lock file

--- src/lib/setup.js
+++ src/lib/setup.js
@@ -1,5 +1,4 @@
 require('@sapphire/plugin-logger/register');
-require('@sapphire/plugin-api/register');
 require('@sapphire/plugin-editable-commands/register');
 require('@sapphire/plugin-subcommands/register');
 const { ApplicationCommandRegistries, RegisterBehavior } = require('@sapphire/framework');
