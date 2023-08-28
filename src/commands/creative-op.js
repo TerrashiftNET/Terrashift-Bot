c0069646f6188a5cf85d32af387c8eeffd9b1f08
Tyler Denman <tylerdenman200@gmail.com>
2023-08-28 15:11:07 +0100

    Update server reference

--- src/commands/creative-op.js
+++ src/commands/creative-op.js
@@ -1,6 +1,6 @@
 const { EmbedBuilder } = require('@discordjs/builders');
 const { Command } = require('@sapphire/framework');
-const { server_id, ptero_token, api_url } = require('../config.json');
+const { creative_server_id, ptero_token, api_url } = require('../config.json');
 const https = require('https');
 const Nodeactyl = require('nodeactyl');
 const client = new Nodeactyl.NodeactylClient(api_url, ptero_token);
