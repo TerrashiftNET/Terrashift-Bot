2e70b5ea07e667bfc217b55e24b77d43f3d9b6ec
Tyler Denman <tylerdenman200@gmail.com>
2023-11-04 16:05:54 +0000

    Update restart.js

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -40,7 +40,7 @@ class UserCommand extends Command {
 
 		const farmsData = JSON.parse(data);
 
-		await interaction.reply({ embeds: new EmbedBuilder().setTitle('Replacing blocks...').setDescription(`Preparing to restart by placing blocks and waiting 30 seconds`)})
+		await interaction.reply({ embeds: [new EmbedBuilder().setTitle('Replacing blocks...').setDescription(`Preparing to restart by placing blocks and waiting 30 seconds`)]})
 
 		await Promise.all(
 			
