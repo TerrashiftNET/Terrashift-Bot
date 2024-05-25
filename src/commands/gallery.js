7e68fc2150330f478fa5d1b2a017ea9a1d9ed2be
tylerguy <tylerdenman200@gmail.com>
2024-05-25 12:20:42 +0100

    chore: Remove unsupported image types check in gallery command

--- src/commands/gallery.js
+++ src/commands/gallery.js
@@ -66,13 +66,13 @@ class UserCommand extends Command {
 
 		const filename = path.basename(imgurl).split('?')[0];
 		const ext = path.basename(imgurl).split('.').pop();
-		if (!['png', 'jpg', 'jpeg'].includes(ext)) {
-			await interaction.reply({
-				content: `Only PNG and JPG images are supported!`,
-				ephemeral: true
-			});
-			return;
-		}
+		// if (!['png', 'jpg', 'jpeg'].includes(ext)) {
+		// 	await interaction.reply({
+		// 		content: `Only PNG and JPG images are supported!`,
+		// 		ephemeral: true
+		// 	});
+		// 	return;
+		// }
 		const filepath = path.join(__dirname, `../gallery/${season}/${filename}`);
 
 		const file = fs.createWriteStream(filepath);
