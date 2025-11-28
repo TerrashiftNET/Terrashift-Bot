6298e833eee5329dcd83e5186e7e6906ea5a3e0f
Tyler Denman <tylerdenman200@gmail.com>
2025-11-28 10:38:50 +0000

    Make interaction reply ephemeral

--- src/commands/refresh-skin.js
+++ src/commands/refresh-skin.js
@@ -48,7 +48,7 @@ class UserCommand extends Command {
 
     await client.sendServerCommand(server_id, command);
 
-    await interaction.reply({ embeds: [embed] });
+    await interaction.reply({ embeds: [embed], ephemeral: true });
   }
 }
 
